2c57af17d0423150fd26c078d89e93e570b1afdd
Chris Campbell <chris@ctcampbell.com>
2020-04-09 09:37:01 +0100

    Update api.py

--- helpers/api.py
+++ helpers/api.py
@@ -24,44 +24,19 @@ class VeracodeAPI:
         requests.Session().mount(self.baseurl, HTTPAdapter(max_retries=3))
         self.proxies = proxies
 
-    def _get_request(self, url, params=None):
-        try:
-            session = requests.Session()
-            session.mount(self.baseurl, HTTPAdapter(max_retries=3))
-            request = requests.Request("GET", url, params=params)
-            prepared_request = request.prepare()
-            try:
-                prepared_request.headers["Authorization"] = generate_veracode_hmac_header(urlparse(url).hostname,
-                                                                                          prepared_request.path_url,
-                                                                                          "GET")
-            except VeracodeHMACError:
-                raise VeracodeAPIError("Could not generate API HMAC header")
-            r = session.send(prepared_request, proxies=self.proxies)
-            if 200 >= r.status_code <= 299:
-                if r.content is None:
-                    logging.debug("HTTP response body empty:\r\n{}\r\n{}\r\n{}\r\n\r\n{}\r\n{}\r\n{}\r\n"
-                                  .format(r.request.url, r.request.headers, r.request.body, r.status_code, r.headers, r.content))
-                    raise VeracodeAPIError("HTTP response body is empty")
-                else:
-                    return r.content
-            else:
-                logging.debug("HTTP error for request:\r\n{}\r\n{}\r\n{}\r\n\r\n{}\r\n{}\r\n{}\r\n"
-                              .format(r.request.url, r.request.headers, r.request.body, r.status_code, r.headers, r.content))
-                raise VeracodeAPIError("HTTP error: {}".format(r.status_code))
-        except requests.exceptions.RequestException as e:
-            logging.exception("Connection error")
-            raise VeracodeAPIError(e)
+    def _request(self, url, method, params=None):
+        if method not in ["GET", "POST"]:
+            raise VeracodeAPIError("Unsupported HTTP method")
 
-    def _post_request(self,url,params=None):
         try:
             session = requests.Session()
             session.mount(self.baseurl, HTTPAdapter(max_retries=3))
-            request = requests.Request("POST", url, params=params)
+            request = requests.Request(method, url, params=params)
             prepared_request = request.prepare()
             try:
                 prepared_request.headers["Authorization"] = generate_veracode_hmac_header(urlparse(url).hostname,
                                                                                           prepared_request.path_url,
-                                                                                          "POST")
+                                                                                          method)
             except VeracodeHMACError:
                 raise VeracodeAPIError("Could not generate API HMAC header")
             r = session.send(prepared_request, proxies=self.proxies)
@@ -82,15 +57,15 @@ class VeracodeAPI:
 
     def get_app_list(self):
         """Returns all application profiles."""
-        return self._get_request(self.baseurl + "/4.0/getapplist.do")
+        return self._request(self.baseurl + "/4.0/getapplist.do", "GET")
 
     def get_app_info(self, app_id):
         """Returns application profile info for a given app ID."""
-        return self._get_request(self.baseurl + "/5.0/getappinfo.do", params={"app_id": app_id})
+        return self._request(self.baseurl + "/5.0/getappinfo.do", "GET", params={"app_id": app_id})
 
     def get_sandbox_list(self, app_id):
         """Returns a list of sandboxes for a given app ID"""
-        return self._get_request(self.baseurl + "/5.0/getsandboxlist.do", params={"app_id": app_id})
+        return self._request(self.baseurl + "/5.0/getsandboxlist.do", "GET", params={"app_id": app_id})
 
     def get_build_list(self, app_id, sandbox_id=None):
         """Returns all builds for a given app ID."""
@@ -98,7 +73,7 @@ class VeracodeAPI:
             params = {"app_id": app_id}
         else:
             params = {"app_id": app_id, "sandbox_id": sandbox_id}
-        return self._get_request(self.baseurl + "/4.0/getbuildlist.do", params=params)
+        return self._request(self.baseurl + "/4.0/getbuildlist.do", "GET", params=params)
     
     def get_build_info(self, app_id, build_id, sandbox_id=None):
         """Returns build info for a given build ID."""
@@ -106,11 +81,11 @@ class VeracodeAPI:
             params = {"app_id": app_id, "build_id": build_id}
         else:
             params = {"app_id": app_id, "build_id": build_id, "sandbox_id": sandbox_id}
-        return self._get_request(self.baseurl + "/5.0/getbuildinfo.do", params=params)
+        return self._request(self.baseurl + "/5.0/getbuildinfo.do", "GET", params=params)
 
     def get_detailed_report(self, build_id):
         """Returns a detailed report for a given build ID."""
-        return self._get_request(self.baseurl + "/5.0/detailedreport.do", params={"build_id": build_id})
+        return self._request(self.baseurl + "/5.0/detailedreport.do", "GET", params={"build_id": build_id})
 
     def set_mitigation_info(self,build_id,flaw_id_list,action,comment, results_from_app_id):
         """Adds a new mitigation proposal, acceptance, rejection, or comment for a set of flaws for an application."""
@@ -129,4 +104,4 @@ class VeracodeAPI:
         else:
             action = 'comment'
         payload = {'build_id': build_id, 'flaw_id_list': flaw_id_list, 'action': action, 'comment': comment}
-        return self._post_request(self.baseurl + "/updatemitigationinfo.do", params=payload)
+        return self._request(self.baseurl + "/updatemitigationinfo.do", "POST", params=payload)
